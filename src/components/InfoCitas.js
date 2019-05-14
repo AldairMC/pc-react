4f5d78e12583c71b5ae6761f3662d126de09d8e7
AldairMC <aldairmc2023@gmail.com>
2019-05-13 23:18:45 -0500

    component InfoCitas.js created

---
+++ src/components/InfoCitas.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react';
+
+export default class InfoCitas extends Component { 
+
+    render() {
+
+        const citas = this.props.citas
+
+        const {namePet, nameOwner, timeCite, hoursCite, symptomPet} = citas
+
+        return (
+            <div className="media mb-3">
+                <div className="media-body">
+                    <h2 className="mt-0"></h2>
+                    <h3 className="card-text">{namePet}</h3>
+                    <p className="card-text"><span>Nombre del dueño: </span>{nameOwner}</p>
+                    <p className="card-text"><span>Fecha :</span>{timeCite}</p>
+                    <p className="card-text"><span>Hora: </span>{hoursCite}</p>
+                    <p className="card-text"><span>Sintomas:</span></p>
+                    <p className="card-text">{symptomPet}</p>
+                </div>
+            </div>
+        );
+    }
+}
